c354d02dc2b293db370f3cd747a564c8e1ecdf63
235illino <61979913+235illino@users.noreply.github.com>
2021-05-29 09:14:36 +0300

    Delete devices.sql
    delete

--- devices.sql
+++
@@ -1,7 +0,0 @@
-
-insert into devices_people(device_id, people_id) values (10, 12);
-insert into devices_people(device_id, people_id) values (11, 10);
-insert into devices_people(device_id, people_id) values (12, 11);
-
-
-select d.name, avg(d.price) from devices as d group by d.name;
